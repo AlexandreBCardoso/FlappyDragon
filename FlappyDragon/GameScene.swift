1804aa0c2452deac07e9ef6348d9fa0dba5ddbf8
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2020-05-04 21:03:01 -0300

    - Inclusão de animação de player, rotação e obstáculos.

--- FlappyDragon/GameScene.swift
+++ FlappyDragon/GameScene.swift
@@ -16,7 +16,13 @@ class GameScene: SKScene {
 	var player: SKSpriteNode!
 	var gameArea: CGFloat = 410.0
 	var velocity: Double = 100.0
-    
+	var gameFinished: Bool = false
+	var gameStarted: Bool = false
+	var restard = false
+	var scoreLabel: SKLabelNode!
+	var score: Int = 0
+	var flyForce: CGFloat = 30.0
+	
     override func didMove(to view: SKView) {
 		addBackground()
 		addFloor()
@@ -42,6 +48,20 @@ class GameScene: SKScene {
 		floor.zPosition	= 2
 		floor.position 	= CGPoint(x: floor.size.width/2, y: size.height - gameArea - floor.size.height/2)
 		addChild(floor)
+		
+		let invisibleFloor = SKNode()
+		invisibleFloor.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: size.width, height: 1))
+		invisibleFloor.physicsBody?.isDynamic = false
+		invisibleFloor.position = CGPoint(x: size.width/2, y: size.height - gameArea)
+		invisibleFloor.zPosition = 2
+		addChild(invisibleFloor)
+		
+		let invisibleRoof = SKNode()
+		invisibleRoof.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: size.width, height: 1))
+		invisibleRoof.physicsBody?.isDynamic = false
+		invisibleRoof.position = CGPoint(x: size.width/2, y: size.height)
+		invisibleRoof.zPosition = 2
+		addChild(invisibleRoof)
 	}
 	
 	// Adiciona o imagem de Introdução
@@ -81,11 +101,85 @@ class GameScene: SKScene {
 		let repeatAction = SKAction.repeatForever(sequenciAction)
 		floor.run(repeatAction)
 	}
+	
+	func addScore() {
+		scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+		scoreLabel.fontSize = 94
+		scoreLabel.text = "\(score)"
+		scoreLabel.zPosition = 5
+		scoreLabel.position = CGPoint(x: size.width/2, y: size.height - 100)
+		scoreLabel.alpha = 0.8
+		addChild(scoreLabel)
+	}
+	
+	func spawEnemies() {
+		let initialPosition	= CGFloat(arc4random_uniform(132) + 74)
+		let enemyNumber 	= Int(arc4random_uniform(4) + 1)
+		let enemyDistance 	= self.player.size.height * 2.5
+		let enemyTop 		= SKSpriteNode(imageNamed: "enemytop\(enemyNumber)")
+		let enemyBottom 	= SKSpriteNode(imageNamed: "enemybottom\(enemyNumber)")
+		let enemyWidth 		= enemyTop.size.width
+		let enemyHeight 	= enemyTop.size.height
+		
+		enemyTop.position = CGPoint(x: size.width + enemyWidth/2, y: size.height - initialPosition + enemyHeight/2)
+		enemyTop.zPosition = 1
+		enemyTop.physicsBody = SKPhysicsBody(rectangleOf: enemyTop.size)
+		enemyTop.physicsBody?.isDynamic = false
+		
+		enemyBottom.position = CGPoint(x: size.width + enemyWidth/2, y: enemyTop.position.y - enemyTop.size.height - enemyDistance)
+		enemyBottom.zPosition = 1
+		enemyBottom.physicsBody = SKPhysicsBody(rectangleOf: enemyBottom.size)
+		enemyBottom.physicsBody?.isDynamic = false
+		
+		// Aminação do enemy
+		let distance 		= size.width + enemyWidth
+		let duration 		= Double(distance)/velocity
+		let moveAction 		= SKAction.moveBy(x: -distance, y: 0, duration: duration)
+		let removeAction 	= SKAction.removeFromParent()
+		let sequenceAction	= SKAction.sequence([moveAction, removeAction])
+		
+		enemyTop.run(sequenceAction)
+		enemyBottom.run(sequenceAction)
+		
+		addChild(enemyTop)
+		addChild(enemyBottom)
+		
+		
+	}
     
         
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		if !gameFinished {
+			if !gameStarted {
+				intro.removeFromParent()
+				addScore()
+				gameStarted = true
+				
+				// Criação de um Corpo Físico
+				player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width/2 - 10)
+				// Adicionando gravidade
+				player.physicsBody?.isDynamic = true
+				// Adicionando opção para rotacionar
+				player.physicsBody?.allowsRotation = true
+				// Aplicação de impulso/força
+				player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: flyForce))
+				
+				Timer.scheduledTimer(withTimeInterval: 2.5, repeats: true) { (timer) in
+					self.spawEnemies()
+				}
+				
+			} else {
+				player.physicsBody?.velocity = CGVector.zero
+				// Aplicação de impulso/força
+				player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: flyForce))
+			}
+		}
     }
     
     override func update(_ currentTime: TimeInterval) {
+		if gameStarted {
+			let yVelocity = player.physicsBody!.velocity.dy * 0.001 as CGFloat
+			player.zRotation = yVelocity
+		}
     }
 }
